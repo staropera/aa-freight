abae86f5ef6255d7002049913633505943a05571
ErikKalkoken <erik.kalkoken@gmail.com>
2020-05-15 18:03:56 +0200

    Fix attempt for migration 5

--- freight/__init__.py
+++ freight/__init__.py
@@ -1,4 +1,4 @@
 default_app_config = 'freight.apps.FreightConfig'
 
-__version__ = '1.1.0'
+__version__ = '1.1.1dev1'
 __title__ = 'Alliance Freight'
--- freight/migrations/0005_auto_20191028_2155.py
+++ freight/migrations/0005_auto_20191028_2155.py
@@ -7,8 +7,6 @@ import django.db.models.deletion
 
 class Migration(migrations.Migration):
 
-    atomic = False
-
     dependencies = [
         ('freight', '0004_remove_contract_price'),
     ]
