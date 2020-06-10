30ce961298e704e1e1b8aa75ef2af5c02553ac4b
ErikKalkoken <erik.kalkoken@gmail.com>
2020-06-10 23:40:53 +0200

    Improve esi timeouts

--- freight/app_settings.py
+++ freight/app_settings.py
@@ -1,79 +1,86 @@
 from django.conf import settings
 from .utils import clean_setting
 
-# Name of this app as shown in the Auth sidebar, page titles 
+# Name of this app as shown in the Auth sidebar, page titles
 # and as default avatar name for notifications
-FREIGHT_APP_NAME = \
-    clean_setting('FREIGHT_APP_NAME', 'Alliance Freight', required_type=str)
+FREIGHT_APP_NAME = clean_setting(
+    "FREIGHT_APP_NAME", "Alliance Freight", required_type=str
+)
 
 # Sets the number minutes until a delayed sync will be recognized as error
-FREIGHT_CONTRACT_SYNC_GRACE_MINUTES = \
-    clean_setting('FREIGHT_CONTRACT_SYNC_GRACE_MINUTES', 30)
+FREIGHT_CONTRACT_SYNC_GRACE_MINUTES = clean_setting(
+    "FREIGHT_CONTRACT_SYNC_GRACE_MINUTES", 30
+)
 
 
 # Enables features for developers, e.g. write access to all models in admin
-FREIGHT_DEVELOPER_MODE = \
-    clean_setting('FREIGHT_DEVELOPER_MODE', False)
+FREIGHT_DEVELOPER_MODE = clean_setting("FREIGHT_DEVELOPER_MODE", False)
 
 
 # Webhook URL used for notifications if defined
-FREIGHT_DISCORD_WEBHOOK_URL = \
-    clean_setting('FREIGHT_DISCORD_WEBHOOK_URL', None, required_type=str)
+FREIGHT_DISCORD_WEBHOOK_URL = clean_setting(
+    "FREIGHT_DISCORD_WEBHOOK_URL", None, required_type=str
+)
 
 
 # Will be shown as "user name" instead of what is configured as app name
 # for notifications if defined
-FREIGHT_DISCORD_AVATAR_NAME = \
-    clean_setting('FREIGHT_DISCORD_AVATAR_NAME', None, required_type=str)
+FREIGHT_DISCORD_AVATAR_NAME = clean_setting(
+    "FREIGHT_DISCORD_AVATAR_NAME", None, required_type=str
+)
 
 
 # when set true will no longer set name and avatar for webhooks
-FREIGHT_DISCORD_DISABLE_BRANDING = \
-    clean_setting('FREIGHT_DISCORD_DISABLE_BRANDING', False)
+FREIGHT_DISCORD_DISABLE_BRANDING = clean_setting(
+    "FREIGHT_DISCORD_DISABLE_BRANDING", False
+)
 
 
 # Mentions to appear upfront to any notification
 # Typical values are: None, '@here' and '@everyone'.
-FREIGHT_DISCORD_MENTIONS = \
-    clean_setting('FREIGHT_DISCORD_MENTIONS', None, required_type=str)
+FREIGHT_DISCORD_MENTIONS = clean_setting(
+    "FREIGHT_DISCORD_MENTIONS", None, required_type=str
+)
 
 
 # max days back considered when calculating statistics
-FREIGHT_STATISTICS_MAX_DAYS = \
-    clean_setting('FREIGHT_STATISTICS_MAX_DAYS', 90, min_value=1)
+FREIGHT_STATISTICS_MAX_DAYS = clean_setting(
+    "FREIGHT_STATISTICS_MAX_DAYS", 90, min_value=1
+)
 
 # Webhook URL used for notifications to customers if defined
-FREIGHT_DISCORD_CUSTOMERS_WEBHOOK_URL = \
-    clean_setting('FREIGHT_DISCORD_CUSTOMERS_WEBHOOK_URL', None, required_type=str)
+FREIGHT_DISCORD_CUSTOMERS_WEBHOOK_URL = clean_setting(
+    "FREIGHT_DISCORD_CUSTOMERS_WEBHOOK_URL", None, required_type=str
+)
 
 
 # defines after how many hours a status becomes stale
 # a stale status will not be reported to customers
-FREIGHT_HOURS_UNTIL_STALE_STATUS = \
-    clean_setting('FREIGHT_HOURS_UNTIL_STALE_STATUS', 24)
+FREIGHT_HOURS_UNTIL_STALE_STATUS = clean_setting("FREIGHT_HOURS_UNTIL_STALE_STATUS", 24)
 
 
 # Whether to show full location names in the route dropdown of the calculator
-FREIGHT_FULL_ROUTE_NAMES = \
-    clean_setting('FREIGHT_FULL_ROUTE_NAMES', False)
+FREIGHT_FULL_ROUTE_NAMES = clean_setting("FREIGHT_FULL_ROUTE_NAMES", False)
 
+# whether created timers are corp restricted on the timerboard
+FREIGHT_ESI_TIMEOUT_ENABLED = clean_setting("FREIGHT_ESI_TIMEOUT_ENABLED", True)
 
 # modes of operation for Alliance Freight
-FREIGHT_OPERATION_MODE_MY_ALLIANCE = 'my_alliance'
-FREIGHT_OPERATION_MODE_MY_CORPORATION = 'my_corporation'
-FREIGHT_OPERATION_MODE_CORP_IN_ALLIANCE = 'corp_in_alliance'
-FREIGHT_OPERATION_MODE_CORP_PUBLIC = 'corp_public'
+FREIGHT_OPERATION_MODE_MY_ALLIANCE = "my_alliance"
+FREIGHT_OPERATION_MODE_MY_CORPORATION = "my_corporation"
+FREIGHT_OPERATION_MODE_CORP_IN_ALLIANCE = "corp_in_alliance"
+FREIGHT_OPERATION_MODE_CORP_PUBLIC = "corp_public"
 
 FREIGHT_OPERATION_MODES = [
-    (FREIGHT_OPERATION_MODE_MY_ALLIANCE, 'My Alliance'),
-    (FREIGHT_OPERATION_MODE_MY_CORPORATION, 'My Corporation'),
-    (FREIGHT_OPERATION_MODE_CORP_IN_ALLIANCE, 'Corporation in my Alliance'),
-    (FREIGHT_OPERATION_MODE_CORP_PUBLIC, 'Corporation public'),
+    (FREIGHT_OPERATION_MODE_MY_ALLIANCE, "My Alliance"),
+    (FREIGHT_OPERATION_MODE_MY_CORPORATION, "My Corporation"),
+    (FREIGHT_OPERATION_MODE_CORP_IN_ALLIANCE, "Corporation in my Alliance"),
+    (FREIGHT_OPERATION_MODE_CORP_PUBLIC, "Corporation public"),
 ]
 
-if (hasattr(settings, 'FREIGHT_OPERATION_MODE') 
-    and settings.FREIGHT_OPERATION_MODE in [x[0] for x in FREIGHT_OPERATION_MODES]
-):
+if hasattr(settings, "FREIGHT_OPERATION_MODE") and settings.FREIGHT_OPERATION_MODE in [
+    x[0] for x in FREIGHT_OPERATION_MODES
+]:
     FREIGHT_OPERATION_MODE = settings.FREIGHT_OPERATION_MODE
 else:
     FREIGHT_OPERATION_MODE = FREIGHT_OPERATION_MODE_MY_ALLIANCE
--- freight/helpers/esi_fetch.py
+++ freight/helpers/esi_fetch.py
@@ -19,6 +19,7 @@ from esi.clients import esi_client_factory
 from esi.models import Token
 
 from .. import __title__
+from ..app_settings import FREIGHT_ESI_TIMEOUT_ENABLED
 from ..utils import LoggerAddTag
 
 
@@ -26,7 +27,6 @@ logger = LoggerAddTag(logging.getLogger(__name__), __title__)
 
 ESI_MAX_RETRIES = 3
 ESI_RETRY_SLEEP_SECS = 1
-ESI_API_TIMEOUT = 10
 
 _my_esi_client = None
 
@@ -37,7 +37,7 @@ def esi_fetch(
     has_pages: bool = False,
     token: Token = None,
     esi_client: object = None,
-    logger_tag: str = None
+    logger_tag: str = None,
 ) -> dict:
     """returns an response object from ESI, will retry on some HTTP errors.
     will automatically return all pages if requested
@@ -53,13 +53,13 @@ def esi_fetch(
     - logger_tag: every log message will start with this text in brackets
     """
     _, request_object = _fetch_main(
-        esi_path=esi_path, 
+        esi_path=esi_path,
         args=args,
         languages=None,
         has_pages=has_pages,
         esi_client=esi_client,
         token=token,
-        logger_tag=logger_tag
+        logger_tag=logger_tag,
     ).popitem()
     return request_object
 
@@ -71,7 +71,7 @@ def esi_fetch_with_localization(
     has_pages: bool = False,
     esi_client: object = None,
     token: Token = None,
-    logger_tag: str = None
+    logger_tag: str = None,
 ) -> dict:
     """returns dict of response objects from ESI
     will contain one full object items for each language if supported or just one
@@ -97,7 +97,7 @@ def esi_fetch_with_localization(
         has_pages=has_pages,
         esi_client=esi_client,
         token=token,
-        logger_tag=logger_tag
+        logger_tag=logger_tag,
     )
 
 
@@ -108,30 +108,30 @@ def _fetch_main(
     has_pages: bool,
     esi_client: object,
     token: Token,
-    logger_tag: str
+    logger_tag: str,
 ) -> dict:
     """returns dict of response objects from ESI with localization"""
 
     if not args:
         args = {}
-    
+
     if not languages:
         has_localization = False
-        languages = {'dummy'}
+        languages = {"dummy"}
     else:
         has_localization = True
 
     response_objects = dict()
     for language in languages:
         if has_localization:
-            args['language'] = language
+            args["language"] = language
         response_objects[language] = _fetch_with_paging(
-            esi_path=esi_path, 
+            esi_path=esi_path,
             args=args,
             has_pages=has_pages,
             esi_client=esi_client,
             token=token,
-            logger_tag=logger_tag
+            logger_tag=logger_tag,
         )
 
     return response_objects
@@ -143,29 +143,29 @@ def _fetch_with_paging(
     has_pages: bool = False,
     esi_client: object = None,
     token: Token = None,
-    logger_tag: str = None
+    logger_tag: str = None,
 ) -> dict:
-    """fetches esi objects incl. all pages if requested and returns them""" 
+    """fetches esi objects incl. all pages if requested and returns them"""
     response_object, pages = _fetch_with_retries(
-        esi_path=esi_path, 
-        args=args, 
-        has_pages=has_pages, 
+        esi_path=esi_path,
+        args=args,
+        has_pages=has_pages,
         esi_client=esi_client,
         token=token,
-        logger_tag=logger_tag
-    )        
+        logger_tag=logger_tag,
+    )
     if has_pages:
         for page in range(2, pages + 1):
             response_object_page, _ = _fetch_with_retries(
-                esi_path=esi_path, 
+                esi_path=esi_path,
                 args=args,
                 has_pages=has_pages,
                 page=page,
                 pages=pages,
                 esi_client=esi_client,
                 token=token,
-                logger_tag=logger_tag
-            )  
+                logger_tag=logger_tag,
+            )
             response_object += response_object_page
 
     return response_object
@@ -176,9 +176,9 @@ def _esi_client() -> object:
     global _my_esi_client
 
     if not _my_esi_client:
-        logger.info('Initializing esi client for esi_fetch....')
+        logger.info("Initializing esi client for esi_fetch....")
         _my_esi_client = esi_client_factory()
-    
+
     return _my_esi_client
 
 
@@ -190,7 +190,7 @@ def _fetch_with_retries(
     pages: int = None,
     esi_client: object = None,
     token: Token = None,
-    logger_tag: str = None
+    logger_tag: str = None,
 ) -> tuple:
     """Returns response object and pages from ESI, retries on 502s"""
 
@@ -224,40 +224,36 @@ def _prepare_esi_request(
     token: Token = None,
 ):
     """parses and validates input for esi request"""
-    esi_path_parts = esi_path.split('.')
+    esi_path_parts = esi_path.split(".")
     if len(esi_path_parts) != 2:
-        raise ValueError('Invalid esi_path')
+        raise ValueError("Invalid esi_path")
     esi_category_name = esi_path_parts[0]
     esi_method_name = esi_path_parts[1]
     if not esi_client:
         esi_client = _esi_client()
     if not hasattr(esi_client, esi_category_name):
-        raise ValueError(
-            'Invalid ESI category: %s' % esi_category_name
-        )
+        raise ValueError("Invalid ESI category: %s" % esi_category_name)
     esi_category = getattr(esi_client, esi_category_name)
     if not hasattr(esi_category, esi_method_name):
         raise ValueError(
-            'Invalid ESI method for %s category: %s'
+            "Invalid ESI method for %s category: %s"
             % (esi_category_name, esi_method_name)
         )
-    log_message_base = 'Fetching from ESI: {}'.format(esi_path)
+    log_message_base = "Fetching from ESI: {}".format(esi_path)
     if settings.DEBUG:
-        log_message_base += '({})'.format(
-            ', '.join([str(k) + '=' + str(v) for k, v in args.items()])
+        log_message_base += "({})".format(
+            ", ".join([str(k) + "=" + str(v) for k, v in args.items()])
         )
     if has_pages:
         if not page:
             page = 1
-        args['page'] = page
-        log_message_base += ' - Page {}/{}'.format(
-            page, pages if pages else '?'
-        )
+        args["page"] = page
+        log_message_base += " - Page {}/{}".format(page, pages if pages else "?")
     if token:
         if token.expired:
             token.refresh()
-        args['token'] = token.access_token
-    
+        args["token"] = token.access_token
+
     return esi_category, esi_method_name, log_message_base
 
 
@@ -277,44 +273,49 @@ def _execute_esi_request(
     logger.info(add_prefix(log_message_base))
     for retry_count in range(ESI_MAX_RETRIES + 1):
         if retry_count > 0:
-            logger.warn(add_prefix(
-                '{} - Retry {} / {}'.format(
-                    log_message_base, retry_count, ESI_MAX_RETRIES
+            logger.warn(
+                add_prefix(
+                    "{} - Retry {} / {}".format(
+                        log_message_base, retry_count, ESI_MAX_RETRIES
+                    )
                 )
-            ))
-        try:                  
+            )
+        try:
             operation = getattr(esi_category, esi_method_name)(**args)
+            result_args = {"timeout": (5, 30)} if FREIGHT_ESI_TIMEOUT_ENABLED else {}
             if has_pages:
                 operation.also_return_response = True
-                response_object, response = operation.result(timeout=ESI_API_TIMEOUT)
-                if 'x-pages' in response.headers:
-                    pages = int(response.headers['x-pages'])
+                response_object, response = operation.result(**result_args)
+                if "x-pages" in response.headers:
+                    pages = int(response.headers["x-pages"])
                 else:
                     pages = 0
             else:
-                response_object = operation.result()
+                response_object = operation.result(**result_args)
                 pages = 0
             break
 
         except (HTTPBadGateway, HTTPGatewayTimeout, HTTPServiceUnavailable) as ex:
-            logger.warn(add_prefix(
-                'HTTP error while trying to '
-                'fetch response_object from ESI: {}'.format(ex)
-            ))
+            logger.warn(
+                add_prefix(
+                    "HTTP error while trying to "
+                    "fetch response_object from ESI: {}".format(ex)
+                )
+            )
             if retry_count < ESI_MAX_RETRIES:
                 sleep_seconds = (ESI_RETRY_SLEEP_SECS * retry_count) ** 2
-                logger.info(add_prefix(
-                    'Waiting {} seconds until next retry'.format(sleep_seconds)
-                ))
+                logger.info(
+                    add_prefix(
+                        "Waiting {} seconds until next retry".format(sleep_seconds)
+                    )
+                )
                 sleep(sleep_seconds)
             else:
                 raise ex
-    
+
     return response_object, pages
 
 
 def _make_logger_prefix(tag: str = None):
     """creates a function to add logger prefix"""
-    return lambda text: '{}{}'.format(
-        (tag + ': ') if tag else '', text
-    )
+    return lambda text: "{}{}".format((tag + ": ") if tag else "", text)
